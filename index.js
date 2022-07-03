635019a8175e474094948953e058fd9e23d405b6
Florian Regnery <floregnery97@gmail.com>
2022-07-03 13:09:08 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// MIDDLEWARE
+
+// ROUTES
+
+// USING MIDDLEWARE
+
+// USING ROUTES
+
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`)
+})
