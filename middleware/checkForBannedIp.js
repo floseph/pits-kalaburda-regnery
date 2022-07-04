7ed6190bf2c759e36551745ec540026cd711db75
Florian Regnery <floregnery97@gmail.com>
2022-07-04 14:02:18 +0200

    bannedips now in data.db instead of json

--- middleware/checkForBannedIp.js
+++ middleware/checkForBannedIp.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
+const database = require('../model/database')
 
-function checkForBannedIps(req, res, next){
+async function checkForBannedIps(req, res, next){
   
   const ip = req.ip
-  const bannedIps = JSON.parse(fs.readFileSync(`${__dirname}/../json/bannedIps.json`))
+  // const bannedIps = JSON.parse(fs.readFileSync(`${__dirname}/../json/bannedIps.json`))
+  const bannedIps = await database.selectAllIps()
 
   if(bannedIps.includes(ip)){
     res.status(403)
